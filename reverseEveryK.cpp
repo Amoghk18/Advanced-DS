51c2226dee07ee31220e7058aac6e1008d42baf2
Amoghk18 <53806570+Amoghk18@users.noreply.github.com>
2020-09-16 18:20:21 +0530

    Update reverseEveryK.cpp

--- reverseEveryK.cpp
+++ reverseEveryK.cpp
@@ -21,63 +21,6 @@ void printit(Node* head){
     }
 }
 
-Node* revLL(Node* start, Node* end){
-    Node* curr = start;
-    Node* prev = NULL;
-    Node* next = NULL;
-    while (curr != end)
-    {
-        next = curr->next;
-        curr->next = prev;
-        prev = curr;
-        curr = next;
-    }
-    start = prev;
-    return start;
-}
-
-Node* revK(Node* head, int k){
-    int cnt = 0;
-    Node* curr = head;
-    Node* ohead = head;
-    Node* newHead = NULL;
-    Node* retHead = NULL;
-    int reverseCnt = 0;
-    Node* prevTempHead = NULL;
-    Node* arr[2];
-    while(curr != NULL){
-        cnt++;
-        if(cnt % k == 0){
-            Node* tempHead = ohead;
-            Node* tempNext = curr->next;
-            cout<< cnt << " " << curr->val << endl;
-            newHead = revLL(ohead, curr->next);
-            arr[reverseCnt] = newHead;
-            reverseCnt++;
-            if(reverseCnt == 1){
-                retHead = newHead;
-            }
-            prevTempHead->next = newHead;
-            tempHead->next = tempNext;
-            curr = tempNext;
-            ohead = tempNext;
-            prevTempHead = tempHead;
-            continue;
-        }
-        curr = curr->next;
-    }
-    // Node *newList = arr[0];
-    // for(int i=0; i < 1;i++){
-    //     Node* p = newList;
-    //     while(p->next != NULL){
-    //         p = p->next;
-    //     }
-    //     p->next = arr[i+1];
-    // }
-
-    return retHead;
-}
-
 Node* reverseit (Node *head, int k)  
 {  
     Node* current = head;  
